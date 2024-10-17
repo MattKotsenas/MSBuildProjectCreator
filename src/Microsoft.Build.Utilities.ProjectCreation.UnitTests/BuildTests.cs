1a89d8193a63e72eaa1de4e068aaf970e8904e08
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-10-17 14:03:33 -0700

    Replace Exec ping with Thread.Sleep

--- src/Microsoft.Build.Utilities.ProjectCreation.UnitTests/BuildTests.cs
+++ src/Microsoft.Build.Utilities.ProjectCreation.UnitTests/BuildTests.cs
@@ -143,13 +143,11 @@ namespace Microsoft.Build.Utilities.ProjectCreation.UnitTests
             {
                 projects.Add(
                     ProjectCreator.Create(GetTempProjectPath())
+                        .UsingTaskRoslynCode(
+                            taskName: "Sleep",
+                            code: "System.Threading.Thread.Sleep(2000);")
                         .Target("Build")
-                        .Task(
-                            "Exec",
-                            parameters: new Dictionary<string, string?>
-                            {
-                                ["Command"] = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "ping 127.0.0.1 -n 2 >NUL" : "sleep 2",
-                            })
+                        .Task("Sleep")
                         .Save());
             }
 
